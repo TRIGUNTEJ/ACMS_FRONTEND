5ce42bff474af7b862ceed8b0c9c7fd2d654a49a
TRIGUNTEJ <triguncherry@gmail.com>
2024-07-20 00:06:35 +0530

    files updated

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -5,32 +5,31 @@ import axios from 'axios';
 const RegistrationForm = ({ hidePopup, onSuccess }) => {
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
 
   axios.defaults.withCredentials = true;
 
   const handleRegister = (e) => {
     e.preventDefault();
 
-    axios.post("https://acms-backend-c1vn.onrender.com/register", { email, name, password })
-      .then(req => {
-        console.log(req);
+    axios.post("https://acms-backend-c1vn.onrender.com", { email, name })
+      .then(response => {
+        console.log(response.data);
         onSuccess();
       })
-      .catch(err => console.log(err));
+      .catch(error => {
+        console.error('Error registering user:', error);
+      });
   };
 
   return (
     <div className="registration-form">
       <h2>Registration</h2>
-      <form>
+      <form onSubmit={handleRegister}>
         <label htmlFor="email">Email:</label>
-        <input type="text" id="email" name="email" required onChange={e => setEmail(e.target.value)} /><br />
+        <input type="email" id="email" name="email" required onChange={e => setEmail(e.target.value)} /><br />
         <label htmlFor="name">Name:</label>
         <input type="text" id="name" name="name" required onChange={e => setName(e.target.value)} /><br />
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" required onChange={e => setPassword(e.target.value)} /><br />
-        <button type="submit" className="login-button" onClick={handleRegister}>Register</button>
+        <button type="submit" className="login-button">Register</button>
       </form>
     </div>
   );
