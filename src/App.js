512a05edaed95aceea2a9cdfa391e1f4a9fb1f40
TRIGUNTEJ <triguncherry@gmail.com>
2024-08-06 11:40:54 +0530

    files updated

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Home from './components/home';
 import AdminDashboard from './components/admin/AdminDashboard';
 import AdminEnroll from './components/admin/AdminEnroll'
 import StudentDashboard from './components/StudentDashbard';
-
+import AdminViewCourse from './components/admin/AdminCourseDetails';
+import AdminFaculty from './components/admin/FacultyManagement';
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
           <Route path="/admin-dashboard" element={<AdminDashboard />} />
           <Route path="/student-dashboard" element={<StudentDashboard />} />
           <Route path="/admin-enrollment" element={<AdminEnroll/>} />
+          <Route path="/admin-course-details" element={<AdminViewCourse/>} />
+          <Route path="/admin-faculty" element={<AdminFaculty/>} />
+
         </Routes>
       </div>
     </Router>
